4687a715e91d1a816e7af04fd34538346997bd0e
Sameer Khavanekar <sameer@Sameers-MacBook-Pro.local>
2015-03-05 14:23:56 -0800

    Add click counter and uncommitted project settings files.

--- Projects/Click Counter/Click Counter/ViewController.swift
+++ Projects/Click Counter/Click Counter/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var count = 0
+    var increment:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        InitUI()
     }
+    
+    func InitUI(){
+        increment = UILabel()
+        increment.frame = CGRectMake(150, 150, 60, 60)
+        increment.text = "\(0)"
+        increment.textColor = UIColor.redColor()
+        self.view.addSubview(increment)
+        
+        var incrementButton = UIButton()
+        incrementButton.frame = CGRectMake(150, 200, 60, 60)
+        incrementButton.setTitle("Increment", forState: UIControlState.Normal)
+        incrementButton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        self.view.addSubview(incrementButton)
+        incrementButton.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    func incrementCount(){
+        count += 1
+        increment.text = "\(count)"
+    }
 }
 
